57b3e190338c94d4ba1297c2fa026e51790131ed
Nguyễn Trần Phước Thành <thanh.nguyen7921@hcmut.edu.vn>
2024-05-15 22:47:09 +0700

    Add generation for program content header file
    These C header files are used by the PL to load program into BRAM.

--- Hubris/DebugProgram/assemble/compile.sh
+++ Hubris/DebugProgram/assemble/compile.sh
@@ -4,4 +4,7 @@
 riscv32-unknown-elf-gcc -T link.ld assembly.s -o assembly.o \
 	-march=rv32i -mabi=ilp32 -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g &&
 	riscv32-unknown-elf-objcopy -O verilog \
-		--only-section='.text*' --only-section='.data*' assembly.o assembly.load
+		--only-section='*' assembly.o assembly.mem &&
+	riscv32-unknown-elf-objcopy -O binary \
+		--only-section='*' assembly.o assembly.bin &&
+	xxd -i assembly.bin >program_content.h
--- Hubris/DebugProgram/assemble/run.sh
+++ Hubris/DebugProgram/assemble/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # run command
-hubris +bin=assembly.load +regstat=regstat.json +memdump=mem.bin +sigdebug
+hubris +bin=assembly.mem +regstat=regstat.json +memdump=mem.bin +sigdebug
